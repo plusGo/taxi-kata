eda8e1e888d79d7dbdedc90cd204597ac9fc1e12
menghonglin <mhlplus@163.com>
2019-08-27 14:05:20 +0800

    project init

---
+++ src/test/java/kata/taxi/AppTest.java
@@ -0,0 +1,12 @@
+package kata.taxi;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class AppTest {
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue( true );
+    }
+}
